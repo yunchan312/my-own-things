77e96f126a49b58c3afff669ddabeaa2478c1b30
yunchan312 <phenomenal312@naver.com>
2020-09-25 00:22:11 +0900

    first commit

---
+++ 소스.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int something, answer = 1;
+	cin >> something;
+
+	for (int i = 0; i < 10; i++)
+	{
+		answer *= something;
+	}
+
+	cout << answer;
+
+	return 0;
+}
